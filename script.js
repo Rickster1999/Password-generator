a45e55b08f3386e4fb8a462ab46d91b4d603e1b7
Ricky <ricky15potter@gmail.com>
2023-05-02 18:45:16 -0500

    Added confirm function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Added confirm function
+function generatePassword(){
+  let uppercase = confirm("Would you like uppercase?")
+  let lowercase = confirm("Would you like lowercase?")
+  let numbers = confirm("Would you like any numbers?")
+  let specialCharacters = confirm("Would you like and special characters?")
+}
 
 // Write password to the #password input
 function writePassword() {
